494917a7f247dc5532a6cfce723d6f0d22cee373
shiradofu <43514606+shiradofu@users.noreply.github.com>
2022-03-28 00:48:17 +0900

    modify tail

--- tail.c
+++ tail.c
@@ -1,13 +1,13 @@
-/* head with getopt */
 #include <stdio.h>
 #include <stdlib.h>
-#define _GNU_SOURCE
 #include <getopt.h>
 
 static void do_tail(FILE *f, long nlines);
+static int asc(const void *a, const void *b);
 static void help_msg(char *cmd, int code);
 
 #define DEFAULT_N_LINES 10
+#define SIZEOF_ARR(array) (sizeof(array)/sizeof(array[0]))
 
 static struct option longopts[] = {
   {"lines", required_argument, NULL, 'n'},
@@ -54,33 +54,36 @@ static void
 do_tail(FILE *f, long nlines)
 {
   long cnt = 0;
-  unsigned long newline_byteno[nlines];
+  unsigned long newline_byteno[nlines + 1];
+  int i;
   int c;
+  char prev = '\n';
 
   if (nlines <= 0) return;
+
+  for (i = 0; i < nlines + 1; i++) newline_byteno[i] = 0;
 
   while((c = getc(f)) != EOF) {
     if (c == '\n') {
       cnt++;
-      if (nlines == 1) {
-        newline_byteno[0] = ftello(f);
-      } else if (cnt <= nlines) {
-        newline_byteno[cnt - 1] = ftello(f);
-      } else {
-        int i;
-        for (i = 0; i < nlines - 1; i++) {
-          newline_byteno[i] = newline_byteno[i + 1];
-        }
-        newline_byteno[nlines - 1] = ftello(f);
-      }
+      newline_byteno[cnt % (nlines + 1)] = ftello(f);
     }
+    prev = c;
   }
   
-  fseeko(f, newline_byteno[0] + 1, SEEK_SET);
-
+  qsort(newline_byteno, SIZEOF_ARR(newline_byteno), sizeof(unsigned long), asc);
+  fseeko(f, prev == '\n' ? newline_byteno[0] : newline_byteno[1], SEEK_SET);
   while((c = getc(f)) != EOF) {
     if (putchar(c) < 0) exit(1);
   }
+}
+
+static int
+asc(const void *a, const void *b)
+{
+  int na = *(int*)a;
+  int nb = *(int*)b;
+  return na - nb;
 }
 
 static void
